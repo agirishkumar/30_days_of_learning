e5f74fe97123f7012f10ca2886e23c00f28ef54e
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-12 21:14:04 +0530

    serverup

---
+++ node-app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+// function rqListener(req, res) {
+
+// };
+
+const server = http.createServer((req,res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My First Page </title></head>');
+    res.write('<body><h1>Hello from my Node server </h1></body>');
+    res.write('</html>');
+});
+
+server.listen(3000);
