b66455f08eb2ff303ff084be8b209f530edfc4cd
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-15 16:30:45 +0530

    routing

--- node-app.js
+++ node-app.js
@@ -4,8 +4,17 @@ const http = require('http');
 // };
 
 const server = http.createServer((req,res) => {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    const method =req.method;;
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write("</html>");
+        return res.end();
+
+
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> My First Page </title></head>');
