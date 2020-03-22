c0278636ad235d2a8efcc5bce7f172bdca6ebc83
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-22 18:39:27 +0530

    commit

--- expressapp/app.js
+++ expressapp/app.js
@@ -12,6 +12,7 @@ app.use((req ,res ,next) => {
 });
 app.use((req ,res ,next) => {
     console.log("In the third Middleware");
+    res.send('<h1>Hello from Express</h1>');
 });
 const server = http.createServer(app);
 server.listen(3000);
