5b1ff40eba7cd482827860470ad1c9f908a9704c
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-26 15:12:48 +0530

    alternative to http module

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,18 +1,11 @@
-const http =  require('http'); 
+// const http =  require('http'); 
 const express = require('express');
-
 const app = express();
-app.use((req ,res ,next) => {
-    console.log("In the first Middleware");
-    next();
-});
-app.use((req ,res ,next) => {
-    console.log("In the second Middleware");
-    next();
-});
+
 app.use((req ,res ,next) => {
     console.log("In the third Middleware");
     res.send('<h1>Hello from Express</h1>');
 });
-const server = http.createServer(app);
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
